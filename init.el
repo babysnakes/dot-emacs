12201aaf14f49e2cc9790e8ef0591ed8daab6d22
Haim Ashkenazi <haim@babysnakes.org>
2012-11-26 21:36:18 +0200

    Added 'custom-file' setting.

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; This file should be ignored by git.
+(setq custom-file
+      (concat user-emacs-directory "custom.el"))
+
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
