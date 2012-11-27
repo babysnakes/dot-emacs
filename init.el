23ed735ef1a106a55a1bbbc3bee5b1a1a033a54a
Haim Ashkenazi <haim@babysnakes.org>
2012-11-27 09:39:10 +0200

    Added chef-mode (there's not package to it).

--- init.el
+++ init.el
@@ -7,5 +7,12 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+(setq custom-vendor-dir (concat user-emacs-directory "vendor/"))
+
+;; chef mode
+(add-to-list 'load-path (concat custom-vendor-dir "chef-mode"))
+(require 'chef-mode)
+(setq chef-use-rvm nil)
+
 ;; enabled disabled commands
 (put 'dired-find-alternate-file 'disabled nil)
