b98e16451888e587560e2b64cf1396e7c87474ae
Haim Ashkenazi <haim@babysnakes.org>
2013-10-24 12:53:07 +0300

    Fixes to the javascript configuration:
    - Reverted to default indentation on js3-mode.
    - Fixed reference to js2-mode.

--- init-javascript.el
+++ init-javascript.el
@@ -4,17 +4,6 @@
 (add-to-list 'load-path (concat custom-vendor-dir "js3-mode"))
 (require 'js3-mode)
 
-;;--------------------------------------------------------------
-;; js3-mode: js3-lazy-commas. see:
-;; https://github.com/thomblake/js3-mode/wiki/AlternateStyles
-;;--------------------------------------------------------------
-(custom-set-variables
- '(js3-lazy-commas t)
- '(js3-expr-indent-offset 2)
- '(js3-paren-indent-offset 2)
- '(js3-square-indent-offset 2)
- '(js3-curly-indent-offset 2))
-
 ;; Mostly copied from starter-kit-js, settings to enable when using js3-mode.
 (eval-after-load 'js3-mode
   '(progn (define-key js3-mode-map "{" 'paredit-open-curly)
@@ -23,7 +12,7 @@
           ;; required? - fixes problem with pretty function font-lock
           ;; (define-key js3-mode-map (kbd ",") 'self-insert-command)
           (font-lock-add-keywords
-           'js2-mode `(("\\(function *\\)("
+           'js3-mode `(("\\(function *\\)("
                         (0 (progn (compose-region (match-beginning 1)
                                                   (match-end 1) "\u0192")
                                   nil)))))))
