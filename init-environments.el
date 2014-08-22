0c4e4fc48671d0ae8db200d65c7a4b63a536789e
Haim Ashkenazi <haim@babysnakes.org>
2014-08-22 10:11:00 +0300

    Revised custom environments.
    Instead of having to specify manually the environment I created a
    function to each environment. Originally I wanted to create a map where
    the value is the function to run, but I didn't manage to eval the forms
    at boot time.

--- init-environments.el
+++ init-environments.el
@@ -1,18 +1,30 @@
 ;; Different environment for various projects
 
-(defun set-environment (environment)
-  (interactive "xEnvironment to set: ")
-  (pcase environment
-    (`rails (progn (load-theme 'sanityinc-solarized-light)
-                   (setq whitespace-line-column 800)
-                   (custom-title "Rails")
-                   (global-set-key (kbd "C-x v C-g") 'vc-git-grep)))
-    (`clientinfo (progn (load-theme 'sanityinc-tomorrow-blue)
-                        (custom-title "Client Infos")))
-    (`scripts (progn (load-theme 'sanityinc-tomorrow-day)
-                     (custom-title "Scripts")))
-    (`chef (progn (load-theme 'sanityinc-tomorrow-night)
-                  (custom-title "Chef")))
-    (_ (print "no such environment configured!"))))
+(defun set-environment-rails ()
+  (interactive)
+  (progn (load-theme 'sanityinc-solarized-light)
+         (setq whitespace-line-column 800)
+         (custom-title "Rails")
+         (global-set-key (kbd "C-x v C-g") 'vc-git-grep)))
+
+(defun set-environment-clientinfo ()
+  (interactive)
+  (progn (load-theme 'sanityinc-tomorrow-blue)
+         (custom-title "Client Infos")))
+
+(defun set-environment-scripts ()
+  (interactive)
+  (progn (load-theme 'sanityinc-tomorrow-day)
+         (custom-title "Scripts")))
+
+(defun set-environment-chef ()
+  (interactive)
+  (progn (load-theme 'sanityinc-tomorrow-night)
+         (custom-title "Chef")))
+
+(defun set-environment-classifiers ()
+  (interactive)
+  (progn (load-theme 'sanityinc-tomorrow-bright)
+         (custom-title "Classifiers")))
 
 (provide 'init-environments)
