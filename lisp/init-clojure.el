b53dfcf76f628baeafe5736754ba45caa35975cd
Haim Ashkenazi <haim@babysnakes.org>
2015-09-11 21:19:08 +0300

    clj-refactor.

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -4,7 +4,9 @@
 (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook '(lambda ()
-                                (hs-minor-mode +1)))
+                                (hs-minor-mode +1)
+                                (clj-refactor-mode 1)
+                                (cljr-add-keybindings-with-prefix "C-c C-m")))
 
 (eval-after-load 'cider-mode
   '(progn
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -24,6 +24,7 @@
                       clojure-mode
                       cider
                       clojure-cheatsheet
+                      clj-refactor
                       slamhound
 
                       ;; ruby
