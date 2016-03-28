e3631fe2e333ef592490c0a5edbb44806d673f03
Haim Ashkenazi <haim@babysnakes.org>
2016-03-28 23:05:56 +0300

    javascript indentation.

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -5,4 +5,8 @@
 (eval-after-load 'json-mode
   '(define-key json-mode-map (kbd "C-, f j") 'web-beautify-js))
 
+(custom-set-variables
+ '(js-indent-level 2)
+ '(js2-basic-offset 2))
+
 (provide 'init-javascript)
