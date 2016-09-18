375bba666f21ca6d0a8bbfdae0ffe4a8e97c3ddb
Haim Ashkenazi <haim@babysnakes.org>
2016-09-18 14:21:39 +0300

    lilypond pdf open.

--- lisp/init-lilypond.el
+++ lisp/init-lilypond.el
@@ -3,5 +3,6 @@
 (add-to-list 'load-path (concat custom-vendor-dir "lilypond"))
 (load-file (concat custom-vendor-dir "lilypond/lilypond-init.el"))
 (add-to-list 'Info-directory-list (concat custom-vendor-dir "lilypond"))
+(setq LilyPond-pdf-command "open")
 
 (provide 'init-lilypond)
