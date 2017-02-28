56eaa78e21bd050840e33a49714cf305ee9d8991
Haim Ashkenazi <haim@babysnakes.org>
2017-02-28 15:13:55 +0200

    re-install rjsx-mode. sometimes it's better.
    (e.g. with spread operator).

--- init.el
+++ init.el
@@ -390,6 +390,9 @@
   (add-hook 'js2-mode-hook '(lambda ()
                               (electric-pair-mode 1))))
 
+(use-package rjsx-mode
+  :commands rjsx-mode)
+
 (use-package tern
   :commands tern-mode)
 
